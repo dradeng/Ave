57b5128d5ef40589256130618b1fd9ba414c6096
Draden Gaffney <dsg2dm@virginia.edu>
2018-10-28 10:28:54 -0400

    adding range

--- client/src/components/posts/PostFeed.js
+++ client/src/components/posts/PostFeed.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import PostItem from './PostItem';
+import InputRange from 'react-input-range';
 
 class PostFeed extends Component {
-  render() {
-    const { posts } = this.props;
+  constructor(props) {
+  	super(props);
 
-    return posts.map(post => <PostItem className="col-md-6" key={post._id} post={post} />);
+  	this.state = {
+  		value: { min: 0, max: 3000},
+  	};
+  }
+  render() {
+  	
+
+    const { posts } = this.props;
+    let feedContent = posts.map(post => <PostItem className="col-md-6" key={post._id} post={post} />);
+    return (
+    	<div>
+    		<InputRange
+    			step={2}
+    			formatLabel={value => `${value}dollars`}
+    			maxValue={3000}
+    			minValue={0}
+    			draggableTrack={true}
+    			value={this.state.value}
+    			onChange={value => this.setState({ value })}/>
+
+    		{feedContent}
+    	</div>
+    );
   }
 }
 
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -38,7 +38,10 @@ router.post('/uploads', upload.any(), awsWorker.doUpload);
 router.get('/', (req, res) => {
   Post.find()
     .sort({ date: -1 })
-    .then(posts => res.json(posts))
+    .then(posts => {
+      var newPosts = posts.filter((post) => (post.rent > 900 && post.rent < 1100)); 
+      res.json(newPosts);
+    })
     .catch(err => res.status(404).json({ nopostsfound: 'No posts found' }));
 });
 
