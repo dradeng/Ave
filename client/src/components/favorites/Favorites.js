50f664d9dc1ae4727a488d355cc634a6c0883c33
Draden Gaffney <dsg2dm@virginia.edu>
2018-11-03 21:26:09 -0400

    some frontend for favorites, not important

--- client/src/components/favorites/Favorites.js
+++ client/src/components/favorites/Favorites.js
@@ -4,24 +4,37 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../common/Spinner';
 import PostItem from "../posts/PostItem";
+import PostFeed from '../posts/PostFeed';
 import { getCurrentProfile } from '../../actions/profileActions';
+import { getPosts } from '../../actions/postActions';
+
 
 class Favorites extends Component {
+
   componentDidMount() {
+    this.props.getPosts();
     this.props.getCurrentProfile();
   }
 
   render() {
     const { user } = this.props.auth;
     const { profile, loading } = this.props.profile;
-
+    const { posts } = this.props.post; //const { posts, loading } = this.props.post; might need to add loading
     let favoritesContent;
     console.log('LAAAAME');
     console.log(profile);
     if (profile === null || loading) {
       favoritesContent = <Spinner />;
     } else {
-        favoritesContent = profile.favorites.map(post => <p>ONE </p>);
+        favoritesContent = posts.map(post => { 
+            /*if(profile.Favorites.index(post._id)) {
+                return <PostItem className="col-md-6" key={post._id} post={post} />
+            } else {
+                return // nothing
+            }
+            */
+     
+        });
     }
     return (
       <div className="dashboard">
@@ -40,15 +53,17 @@ class Favorites extends Component {
 
 Favorites.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
+  getPost: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   profile: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
+  post: state.post,
   profile: state.profile,
   auth: state.auth
 });
 
-export default connect(mapStateToProps, { getCurrentProfile })(
+export default connect(mapStateToProps, { getCurrentProfile, getPosts })(
   Favorites
 );
